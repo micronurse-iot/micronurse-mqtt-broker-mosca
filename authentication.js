90f118a7bffacfa7cbd30526a441825fb7aab2af
BUPTSSE-Zero <GGGZ-1101-28@Live.cn>
2016-10-15 20:38:29 +0800

    Add topic authorization whose topic has a specific receiver.

--- authentication.js
+++ authentication.js
@@ -60,13 +60,19 @@ function check_permission(client, topic, wr_perm, callback) {
   var async_flag = false;
   var perm_type = 'others_perm';
   var topic_user = undefined;
+  var topic_receiver = undefined;
   var topic_split = topic.split('/');
   if(topic_split.length > 1) {
     topic_user = topic_split[topic_split.length - 1];
+    var end = topic_split.length - 1;
+    if(topic_split.length > 2){
+      topic_receiver = topic_split[topic_split.length - 2];
+      end = topic_split.length - 2;
+    }
     topic = '';
-    for(i = 0; i < topic_split.length - 1; i++){
+    for(i = 0; i < end; i++){
       topic += topic_split[i];
-      if(i < topic_split.length - 2)
+      if(i < end - 1)
         topic += '/';
     }
   }
@@ -82,16 +88,22 @@ function check_permission(client, topic, wr_perm, callback) {
       if(client.user == topic_user)
         perm_type = 'mobile_owner_perm';
       else{
-        async_flag = true;
-        db_util.query_guardianship(topic_user, client.user, function (err, rows, fields) {
-          if(err || rows.length <= 0){
-            if(err)
-              logger.error(err);
-          }else{
-            perm_type = 'mobile_guardian_perm';
+        if(topic_receiver){
+          if(client.user == topic_receiver){
+            perm_type = 'mobile_receiver_perm';
           }
-          check_permission_type(topic, perm_type, wr_perm, client, callback);
-        });
+        }else {
+          async_flag = true;
+          db_util.query_guardianship(topic_user, client.user, function (err, rows, fields) {
+            if (err || rows.length <= 0) {
+              if (err)
+                logger.error(err);
+            } else {
+              perm_type = 'mobile_guardian_perm';
+            }
+            check_permission_type(topic, perm_type, wr_perm, client, callback);
+          });
+        }
       }
       break;
   }
--- init_db.sql
+++ init_db.sql
@@ -6,7 +6,7 @@ CREATE TABLE topic_permission(
   iot_owner_perm VARCHAR(2),
   mobile_owner_perm VARCHAR(2),
   mobile_guardian_perm VARCHAR(2),
-  mobile_friend_perm VARCHAR(2),
+  mobile_receiver_perm VARCHAR(2),
   server_perm VARCHAR(2),
   others_perm VARCHAR(2)
 );
@@ -15,3 +15,5 @@ INSERT INTO topic_permission(topic, iot_owner_perm, mobile_owner_perm, mobile_gu
     VALUES ('sensor_data_report', 'w', 'r', 'r', 'r');
 INSERT INTO topic_permission(topic, mobile_owner_perm, mobile_guardian_perm, server_perm)
     VALUES ('sensor_warning', 'r', 'r', 'w');
+INSERT INTO topic_permission(topic, mobile_owner_perm, mobile_receiver_perm)
+    VALUES ('chatting', 'w', 'r');
